6251833d0e19c198cadfec05841e49a649a07c12
Claptar <binkevich.as@phystech.edu>
2022-05-07 15:48:10 +0300

    Добавил отрисовку анимации

--- computations/pendulum.py
+++ computations/pendulum.py
@@ -1,4 +1,9 @@
 import numpy as np
+from matplotlib.pyplot import style, figure, axes
+from celluloid import Camera
+from IPython import get_ipython
+from tqdm import tqdm
+
 
 # Параметры системы
 t0, T = 0., 14.16
@@ -21,7 +26,7 @@ def f(u, g, m1, m2, l1, l2):
     return f
 
 
-def f_u(u, m1, m2):
+def fu(u, m1, m2):
     f_u = np.zeros((10, 10))
     # Задаются ненулевые компоненты матрицы Якоби
     f_u[0, 4] = 1.
@@ -66,5 +71,47 @@ def solve_ode(f, fu, M, t0, T, initial_val, alpha=(1 + 1j)/2):
     for m in range(M):
         w1 = np.linalg.solve(get_D_matrix() - alpha * tau * fu(u[m], m1, m2), f(u[m], g, m1, m2, l1, l2))
         u[m + 1] = u[m] + tau * w1.real
+    return t, u
+
+
+if __name__ == '__main__':
+    x10, y10 = 3., -4.
+    x20, y20 = 3., -6.
+    vx10, vy10 = 0., 0.
+    vx20, vy20 = 0., 0.
+    lambda10, lambda20 = 100., 100.
+    M = 5000
+
+    initial_val = [x10, y10, x20, y20, vx10, vy10, vx20, vy20, lambda10, lambda20]
+    t, u = solve_ode(f, fu, M, t0, T, initial_val, alpha=(1 + 1j)/2)
+
+    #get_ipython().run_line_magic('matplotlib', 'qt')
+
+    style.use('dark_background')
+    fig = figure()
+    camera = Camera(fig)
+    ax = axes(xlim=(-5.5, 5.5), ylim=(-8, 1))
+    ax.set_aspect('equal')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    for m in tqdm(range(M + 1)):
+        ax.plot(0, 0, color="yellow", marker='o', markersize=5)
+        ax.plot((-2, 2), (0, 0), '-', color="white")
+        # Отрисовка подвеса 1
+        ax.plot((0, u[m, 0]), (0, u[m, 1]), color="white")
+        # Отрисовка груза 1
+        ax.plot(u[m, 0], u[m, 1], color="white", marker='o', markersize=12)
+        # Отрисовка подвеса 2
+        ax.plot((u[m, 0], u[m, 2]), (u[m, 1], u[m, 3]), color="white")
+        # Отрисовка груза 2
+        ax.plot(u[m, 2], u[m, 3], color="white", marker='o', markersize=6)
+        # Отрисовка следа груза 1
+        ax.plot(u[:m, 0], u[:m, 1], '-g', linewidth=1)
+        # Отрисовка следа груза 2
+        ax.plot(u[:m, 2], u[:m, 3], '-y', linewidth=1)
+        if m % 10 == 0:  # Сохраняем только каждый десятый кадр
+            camera.snap()
+    animation = camera.animate(interval=15, repeat=False, blit=True)
+    #animation.save('celluloid_minimal.gif')
 
 
