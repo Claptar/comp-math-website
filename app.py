ed79d18375fb7e16374b8cad28358d27e0d33250
makyaro <klykin-2@yandex.ru>
2022-05-09 12:06:52 +0300

    create simple form, css styles and data controller

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 
 import sys
 
@@ -8,6 +8,15 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def main_page():
     return render_template('main_page.html')
+
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    if request.method == "POST":
+        v_1, v_2, v_3, v_4 = request.form['v_1'], request.form['v_2'], request.form['v_3'], request.form['v_4']
+        x_1, x_2, x_3, x_4 = request.form['x_1'], request.form['x_2'], request.form['x_3'], request.form['x_4']
+
+        return redirect('/')
 
 
 if __name__ == '__main__':
