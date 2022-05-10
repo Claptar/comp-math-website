76e7f355dee94ddacddec94d43aac8b7aed1ea2a
makyaro <klykin-2@yandex.ru>
2022-05-10 15:06:58 +0300

    Change coordinates and velocities fields in database to masses and lengths. Delete all others fields except disk_id

--- data_handling/database.py
+++ data_handling/database.py
@@ -1,10 +1,4 @@
-from flask import Flask, abort
 from flask_sqlalchemy import SQLAlchemy
-import os
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
-# db = SQLAlchemy(app)
 
 db = SQLAlchemy()
 
@@ -13,34 +7,18 @@ class Launches(db.Model):
     __tablename__ = 'launches'
 
     id = db.Column(db.Integer, primary_key=True)
-    x1 = db.Column(db.Float)
-    y1 = db.Column(db.Float)
-    x2 = db.Column(db.Float)
-    y2 = db.Column(db.Float)
-    vx1 = db.Column(db.Float)
-    vy1 = db.Column(db.Float)
-    vx2 = db.Column(db.Float)
-    vy2 = db.Column(db.Float)
-    lambda1 = db.Column(db.Float)
-    lambda2 = db.Column(db.Float)
-    interval_num = db.Column(db.Integer)
+    m1 = db.Column(db.Float)
+    m2 = db.Column(db.Float)
+    l1 = db.Column(db.Float)
+    l2 = db.Column(db.Float)
     drive_id = db.Column(db.String(60))
-    demonstration_name = db.Column(db.String(30))
 
-    def __init__(self, x1, y1, x2, y2, vx1, vy1, vx2, vy2, lambda1, lambda2, interval_num, drive_id, demonstration_name):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
-        self.vx1 = vx1
-        self.vy1 = vy1
-        self.vx2 = vx2
-        self.vy2 = vy2
-        self.lambda1 = lambda1
-        self.lambda2 = lambda2
-        self.interval_num = interval_num
+    def __init__(self, m1, m2, l1, l2, drive_id):
+        self.m1 = m1
+        self.m2 = m2
+        self.l1 = l1
+        self.l2 = l2
         self.drive_id = drive_id
-        self.demonstration_name = demonstration_name
 
     @classmethod
     def check_launch(cls, initials):
